17880ab8b6de3d3bcb823fd763348c17ebecaf8a
Patrick Anthony <patrick.anthony124@gmail.com>
2013-06-27 10:05:01 -0400

    Completed Recall app. Add/Edit/Delete to-done

--- recall.rb
+++ recall.rb
@@ -38,4 +38,32 @@ get '/:id' do
   erb :edit
 end
 
+put '/:id' do
+  n = Note.get params[:id]
+  n.content = params[:content]
+  n.complete = params[:complete] ? 1 : 0
+  n.updated_at = Time.now
+  n.save
+  redirect '/'
+end
+
+get '/:id/delete' do
+  @note = Note.get params[:id]
+  @title = "Confirm deletion of note ##{params[:id]}"
+  erb :delete
+end
+
+delete '/:id' do
+  n = Note.get params[:id]
+  n.destroy
+  redirect '/'
+end
+
+get '/:id/complete' do
+  n = Note.get params[:id]
+  n.complete = n.complete ? 0 : 1 # flip it
+  n.updated_at = Time.now
+  n.save
+  redirect '/'
+end
 
