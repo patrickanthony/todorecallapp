e04ded265b28a5f8db9936466f10fdd9a08b8265
Patrick Anthony <patrick.anthony124@gmail.com>
2013-06-27 09:11:13 -0400

    Ability to edit and delete notes added.

--- recall.rb
+++ recall.rb
@@ -23,4 +23,19 @@ get '/' do
   erb :home
 end
 
+post '/' do
+  n = Note.new
+  n.content = params[:content]
+  n.created_at = Time.now
+  n.updated_at = Time.now
+  n.save
+  redirect '/'
+end
+
+get '/:id' do
+  @note = Note.get params[:id]
+  @title = "Edit note ##{params[:id]}"
+  erb :edit
+end
+
 
